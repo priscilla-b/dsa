d5d0ec7b82d694f7855d3e45afb651a79b1976e0
Priscilla Acheamfour Baah <priscilla.abaah@cbg.com.gh>
2024-07-09 10:05:23 +0000

    update selection sort algo

--- DSA/algorithms/selection-sort/selection-sort.py
+++ DSA/algorithms/selection-sort/selection-sort.py
@@ -4,15 +4,36 @@ def find_smallest(arr):
     for i in range(1, len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
-            smallest_index = [i]
-        return smallest_index
-    
-def selection_sort(arr):
-    newArr = []
-    for i in range(len(arr)):
-        smallest = find_smallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
+            smallest_index = i
+    return smallest_index
 
-print(selection_sort([5, 3, 6, 2, 10]))
+
+def find_largest(arr):
+    largest = arr[0]
+    largest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] > largest:
+            largest = arr[i]
+            largest_index = i
+    return largest_index
+    
+
+
+def selection_sort(arr, order):
+    newArr = []
+    if order == "asc":
+        for i in range(len(arr)):
+            index = find_smallest(arr) 
+            newArr.append(arr.pop(index))
+   
+    else:
+        for i in range(len(arr)):
+            index = find_largest(arr) 
+            newArr.append(arr.pop(index)) 
+
+        
+    return newArr
+      
+
+print(selection_sort([5, 3, 6, 2, 10], "desc"))
         
