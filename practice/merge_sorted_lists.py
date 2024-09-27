da32f489c8a365ed81d0cd0d54232de2e5a8fe2a
Priscilla Acheamfour Baah <priscilla.abaah@cbg.com.gh>
2024-09-27 10:54:49 +0000

    solution starter

--- practice/merge_sorted_lists.py
+++ practice/merge_sorted_lists.py
@@ -1,20 +1,10 @@
-class Node:
-    # creating a node
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-
-    # insertion at beginning of node
-    def insert_at_start(self, data):
-        new_node = Node(data)  # create a new node with the given data
-        if self.head is None:
-            self.head = new_node
-            return
-        else:
-            new_node.next = self.head
-            self.head = new_node
-
-node = Node(30)
-node.insert_at_start(12)
-print(node)
+        
+        pass
