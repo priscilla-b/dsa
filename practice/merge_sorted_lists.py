884cb8b7d7fac2ae9b8cb1e3eefd7b202f01b3d6
Priscilla Baah <pyfbaah@gmail.com>
2024-10-07 07:48:41 +0000

    update

--- practice/merge_sorted_lists.py
+++ practice/merge_sorted_lists.py
@@ -3,8 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        
-        pass
+class Solution:
+    def merge_two_lists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        node = ListNode()
+        current = node
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2.next = list2
+            
+            current = current.next
+
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+
+        return node.next
+
+     
+
+
+solution = Solution()
+solution.merge_two_lists([1, 2, 4], [1, 3, 4])
