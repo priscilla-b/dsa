5c73d11a5be2acbac6bb17b07ec617454414e820
Priscilla Baah <pyfbaah@gmail.com>
2024-10-07 07:18:45 +0000

    update solution

--- practice/roman_to_integer.py
+++ practice/roman_to_integer.py
@@ -1,8 +1,13 @@
 class Solution(object):
+    
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
+        This method has an O(n^2) time complexity.
+        There are O(n) recursive calls, and
+        Each recursive call involves a string slice that also
+        take O(n) time complexity
         """
 
         r_dict = {
@@ -35,7 +40,76 @@ class Solution(object):
         else:
             return num + self.helper(s[1:], r_dict)
 
+    
+    def roman_to_int(self, s):
+        """
+        Using simple iteration
+        """
+        r_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        num = 0
+        for i in range(len(s)-1):
+            first = r_dict[s[i]]
+            if len(s) == 1:
+                return first
+       
+            if first < r_dict[s[i+1]]:
+                num -= first
+            else:
+                num += first
+
+        #   add the last number to the final result since it was not included 
+        # in the iteration
+        return num + r_dict[s[-1]]
+
+    def roman_to_int2(self, s):
+        """
+        Using a for .. in for iteration
+        """
+        r_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        num = 0
+        # keep track of the previous number outside the loop
+        # so that it can be compared to the current number
+        # whenever a new loop starts
+        prev_num = 0
+        # work with the string backwards
+        for char in s[::-1]:
+            current_num = r_dict[char]
+            if current_num >= prev_num:
+                num += current_num
+               
+            else:
+                num -= current_num
+            
+            # update the value of the previous number 
+            # before new loop starts
+            prev_num = current_num
         
+        return num
+         
+
+            
+
+        
+
+
         
 
 
@@ -43,5 +117,9 @@ class Solution(object):
 solution = Solution()
 # print(solution.romanToInt('DCLIX'))
 # print(solution.romanToInt('CLVIII'))
-print(solution.romanToInt('MCMXCIV'))
+# print(solution.romanToInt('MCMXCIV'))
+print(solution.roman_to_int2('LVIII'))
+    
+
+
 
