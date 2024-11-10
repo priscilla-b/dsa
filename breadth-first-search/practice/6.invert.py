a75212326f6ba31d48156d43d4da949413ef3cce
Priscilla Baah <pyfbaah@gmail.com>
2024-11-10 21:06:31 +0000

    add solution

---
+++ breadth-first-search/practice/6.invert.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/invert-binary-tree/
+from collections import deque
+from _helper import Helper
+
+TreeNode = Helper.TreeNode
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        queue = deque([root])
+        while queue:
+            current = queue.popleft()
+            if current:
+
+                current.left, current.right = current.right, current.left
+                
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+        
+           
+            
+        return root
+
+
+helper = Helper()
+solution = Solution()
+root = helper.list_to_binary_tree([4,2,7,1,3,6,9])
+
+solution.invertTree(root)
