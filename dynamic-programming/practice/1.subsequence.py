60b61862d5f9a21b211b9966fc165d380be6413d
Priscilla Baah <pyfbaah@gmail.com>
2024-11-17 00:38:10 +0000

    add explanatory comments

--- dynamic-programming/practice/1.subsequence.py
+++ dynamic-programming/practice/1.subsequence.py
@@ -11,8 +11,14 @@ class Solution:
         for i in range(1, m+1):
             for j in range(1, n+1):
                 if s[i-1] == t[j-1]:
+                    # if the characters match, increment the value of the 
+                    # longest common substring # found so far
                     grid[i][j] = grid[i-1][j-1] + 1
                 else:
+                    # when characters don't match we have two choices: use the LCS value
+                    # before the current s character (grid[i-1][j]) or the one before
+                    # the current t character (grig[i][j-1]). taking the higer of those
+                    # two values helps to keep the logest subsquence value we have so far
                     grid[i][j] = max(grid[i-1][j], grid[i][j-1])
         
         # if s is a subsequence of t, then the max value of the matrix (last cell value of grid)
