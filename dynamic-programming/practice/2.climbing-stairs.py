b80edd6108531089b0e7f3b9baa4a04673b9f412
Priscilla Baah <pyfbaah@gmail.com>
2024-11-17 23:25:18 +0000

    update comments

--- dynamic-programming/practice/2.climbing-stairs.py
+++ dynamic-programming/practice/2.climbing-stairs.py
@@ -4,10 +4,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         """At any point in time, you can only take 1 or 2 steps at a time.
-        So say you're currently at step 0 on your staircase and you want to take
-        4 steps in total, you can take 1 step to step 1 or 2 steps to step 2.
-        At step 1, you can take 1 step to step 2 or 2 steps to step 3. At step 2
-        you can take 2 1 step to step 3 or 2 steps to step 4. 
+        So say you're currently at step0 on your staircase and you want to take
+        4 steps in total, you can take 1 step to step1 or 2 steps to step2.
+        At step1, you can take 1 step to step 2 or 2 steps to step 3. At step2
+        you can take 1 step to step 3 or 2 steps to step 4. 
         At step 3, you can take 1 step to step 4 but if you can't take 2 steps 
         because you're contrained by 4 total steps.
 
